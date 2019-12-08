f32d2edf809fda2789800f85367dc4612a180070
beenish <beeni.khurram@gmail.com>
2019-12-08 14:59:02 +0000

    added in script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+//assigning weatherapi
 var queryUrl = "http://api.weatherapi.com/v1/current.json?key=28b5b18116344d0ba6e131451190812&q=London"
+var $currentTemp = $("#current-temp");
+
+//calling ajax function
 $.ajax ({
     url : queryUrl,
     method : "GET"
-}) . then (function(response) {
-console.log(response)
+}) .then (function(response) {
+  console.log(response)
+  //setting <h5> html tag and assigning response parameters
+  $currentTemp.append($("<h5>").text("Current Temperature: " + response.current.temp_c + "C"));
+  $currentTemp.append($("<h5>").text("Humidity: " + response.current.humidity + "%"));
+  $currentTemp.append($("<h5>").text("Wind Speed: " + response.current.wind_mph + "MPH"));
+  $currentTemp.append($("<h5>").text("UV Index: " + response.current.uv));
 });
